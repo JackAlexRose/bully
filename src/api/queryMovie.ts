061b373b45e79204b51cad13d38b1878a3c4058e
Jack Rose <jackalexanderrose@hotmail.com>
2022-12-06 17:54:15 +0000

    feat: embed builders for movie details

--- src/api/queryMovie.ts
+++ src/api/queryMovie.ts
@@ -1,8 +1,10 @@
 import { get } from "./ApiClient";
 import { Movie } from "../types/movie";
 
-export async function queryMovie(movieId: string): Promise<Movie> {
-  const path = `http://www.omdbapi.com/${movieId}?apikey=${process.env.TMDB_API_KEY}`;
+export async function queryMovie(movieId: number): Promise<Movie> {
+  const path = `http://www.omdbapi.com/${movieId.toString()}?apikey=${
+    process.env.TMDB_API_KEY
+  }`;
   try {
     return await get<Movie>(path);
   } catch (error) {
--- src/commands/movie.ts
+++ src/commands/movie.ts
@@ -1,6 +1,13 @@
 import Command from "../interfaces/Command";
-import { SlashCommandBuilder } from "discord.js";
 import { searchForMovie } from "../api/searchForMovie";
+import { queryMovie } from "../api/queryMovie";
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  EmbedBuilder,
+  SlashCommandBuilder,
+} from "@discordjs/builders";
+import { ButtonStyle } from "discord.js";
 
 export const movie: Command = {
   data: new SlashCommandBuilder()
@@ -15,13 +22,94 @@ export const movie: Command = {
   async execute(interaction) {
     const title = interaction.options.getString("title", true);
     try {
-      const movie = await searchForMovie(title);
-      if (typeof movie === "string") {
-        await interaction.reply(movie);
+      const { results } = await searchForMovie(title);
+
+      if (results.length === 0) {
+        await interaction.reply({
+          content: "No results found.",
+          ephemeral: true,
+        });
       } else {
-        await interaction.reply(
-          `I found ${movie.total_results} movies matching ${title}.`
-        );
+        const movie = results[0];
+
+        const movieDetails = await queryMovie(movie.id);
+
+        const embed = new EmbedBuilder()
+          .setTitle(movieDetails.title)
+          .setDescription(movieDetails.overview || "No overview available.")
+          .setURL(`https://www.themoviedb.org/movie/${movie.id}`)
+          .addFields([
+            {
+              name: "Release Date",
+              value: movieDetails.release_date || "Unknown",
+              inline: true,
+            },
+            {
+              name: "Rating",
+              value: movieDetails.vote_average?.toString() || "Unknown",
+              inline: true,
+            },
+            {
+              name: "Runtime",
+              value: movieDetails.runtime?.toString() || "Unknown",
+              inline: true,
+            },
+            {
+              name: "Genres",
+              value:
+                movieDetails.genres?.map((genre) => genre.name).join(", ") ||
+                "Unknown",
+              inline: false,
+            },
+            {
+              name: "Spoken Languages",
+              value:
+                movieDetails.spoken_languages
+                  ?.map((language) => language.name)
+                  .join(", ") || "Unknown",
+              inline: false,
+            },
+            {
+              name: "Budget",
+              value: movieDetails.budget?.toLocaleString() || "Unknown",
+              inline: true,
+            },
+            {
+              name: "Revenue",
+              value: movieDetails.revenue?.toLocaleString() || "Unknown",
+              inline: true,
+            },
+            {
+              name: "Trailer",
+              value:
+                movieDetails.videos?.results &&
+                movieDetails.videos?.results[0].site === "YouTube" &&
+                movieDetails.videos?.results[0].key !== undefined
+                  ? `https://www.youtube.com/watch?v=${movieDetails.videos?.results[0].key}`
+                  : "No trailer available.",
+            },
+          ])
+          .setImage(
+            `https://image.tmdb.org/t/p/w300${movieDetails.poster_path}`
+          )
+          .setColor(0xffd500);
+
+        const actionRow = new ActionRowBuilder<ButtonBuilder>().addComponents([
+          new ButtonBuilder()
+            .setCustomId("retry")
+            .setLabel("Not the right movie?")
+            .setStyle(ButtonStyle.Primary),
+          new ButtonBuilder()
+            .setCustomId("recommend")
+            .setLabel("I recommend this movie!")
+            .setStyle(ButtonStyle.Success),
+          new ButtonBuilder()
+            .setCustomId("not-recommend")
+            .setLabel("I don't like this movie!")
+            .setStyle(ButtonStyle.Danger),
+        ]);
+
+        await interaction.reply({ embeds: [embed], components: [actionRow] });
       }
     } catch (error) {
       await interaction.reply(
