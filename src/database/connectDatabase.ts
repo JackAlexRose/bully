d3872a5a5ef782db9efba166b5b5975948254792
JackAlexRose <jackalexanderrose@hotmail.com>
2022-12-12 01:22:19 +0000

    fix: dbname structure

--- src/database/connectDatabase.ts
+++ src/database/connectDatabase.ts
@@ -1,10 +1,11 @@
 import { connect } from "mongoose";
 
 export default async () => {
-  await connect(`mongodb://${process.env.MONGO_ADDRESS as string}/bulldogs`, {
+  await connect(`mongodb://${process.env.MONGO_ADDRESS as string}`, {
     authSource: "admin",
     user: process.env.MONGO_USER,
     pass: process.env.MONGO_PASS,
+    dbName: "bulldogs",
   });
   console.log("Database Connected!");
 };
